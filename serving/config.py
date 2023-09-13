19afde173424a636f5af03605f0d236531f420fd
Ian Cecil Mawuli Akoto <iancecilakoto@gmail.com>
2023-09-13 15:43:36 +0000

    code refactored

--- serving/config.py
+++ serving/config.py
@@ -3,21 +3,16 @@ IMAGE_COLLECTION = "COPERNICUS/S2_SR_HARMONIZED"
 SENTINEL_CLOUD_PROB_ID = "COPERNICUS/S2_CLOUD_PROBABILITY"
 DYNAMIC_WORLD_ID = "GOOGLE/DYNAMICWORLD/V1"
 MODIS_LANDCOVER_ID = "MODIS/061/MCD12Q1"
-CLOUD_THRESHOLD = 65 #Define a cloud probability threshold (e.g., 65%).
+CLOUD_THRESHOLD = 65  # Define a cloud probability threshold (e.g., 65%).
 CLOUD_DISPLACEMENT_THRESHOLD = 0.2
-BANDS = [
-    "B2",
-    "B3",
-    "B4",
-    "B8"
-]
+BANDS = ["B2", "B3", "B4", "B8"]
 FEATURES = ["NDVI", "NDWI", "EVI"]
 SCALE = 10
-PATCH_SIZE =5
+PATCH_SIZE = 5
 BATCH_SIZE = 64
 
 
-# For this Project we focus on 3 areas. 
+# For this Project we focus on 3 areas.
 # Change this part if you want to to focus on a different area.
 # For Iran and Sudan, data can span from  july 2019 to 2022
 # for Afghanistan, only data for the whole year 2022 should be used
@@ -28,4 +23,3 @@ SUDAN_START_DATE = "2019-07-01"
 SUDAN_END_DATE = "2022-12-31"
 AFGHANISTAN_START_DATE = "2022-01-01"
 AFGHANISTAN_END_DATE = "2022-12-31"
-
