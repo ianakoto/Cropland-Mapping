2cfd06b738a457fa9e64103bbfd142b5daec08ed
Ian Cecil Mawuli Akoto <iancecilakoto@gmail.com>
2023-09-13 15:42:56 +0000

    aghan dataset sampled to 500

--- serving/data.py
+++ serving/data.py
@@ -4,12 +4,12 @@ import numpy as np
 from config import *
 
 
-countries = ee.FeatureCollection('USDOS/LSIB_SIMPLE/2017')
+countries = ee.FeatureCollection("USDOS/LSIB_SIMPLE/2017")
 
 # Filter countries by name to obtain the ROIs for Iran, Sudan, and Afghanistan
-iran_roi = countries.filter(ee.Filter.eq('country_na', 'Iran'))
-sudan_roi = countries.filter(ee.Filter.eq('country_na', 'Sudan'))
-afghanistan_roi = countries.filter(ee.Filter.eq('country_na', 'Afghanistan'))
+iran_roi = countries.filter(ee.Filter.eq("country_na", "Iran"))
+sudan_roi = countries.filter(ee.Filter.eq("country_na", "Sudan"))
+afghanistan_roi = countries.filter(ee.Filter.eq("country_na", "Afghanistan"))
 
 # Get the geometries of the ROIs
 iran_geometry = iran_roi.geometry()
@@ -17,12 +17,9 @@ sudan_geometry = sudan_roi.geometry()
 afghanistan_geometry = afghanistan_roi.geometry()
 
 
-
-def get_prediction_data(lon, 
-                        lat, 
-                        iran_collection, 
-                        sudan_collection, 
-                        afghanistan_collection):
+def get_prediction_data(
+    lon, lat, iran_collection, sudan_collection, afghanistan_collection
+):
     """Extracts Sentinel image as json at specific lat/lon and timestamp."""
 
     location = ee.Feature(ee.Geometry.Point([lon, lat]))
@@ -34,14 +31,10 @@ def get_prediction_data(lon,
         afghanistan_geometry,
         iran_collection,
         sudan_collection,
-        afghanistan_collection
+        afghanistan_collection,
     )
 
-
-    image = (
-        selected_collection
-        .mosaic()
-    )
+    image = selected_collection.mosaic()
 
     feature = image.neighborhoodToArray(ee.Kernel.square(PATCH_SIZE)).sampleRegions(
         collection=ee.FeatureCollection([location]), scale=SCALE
@@ -89,13 +82,10 @@ def labeled_feature(row, iran_collection, sudan_collection, afghanistan_collecti
         afghanistan_geometry,
         iran_collection,
         sudan_collection,
-        afghanistan_collection
+        afghanistan_collection,
     )
 
-    image = (
-        selected_collection
-        .mosaic()
-    )
+    image = selected_collection.mosaic()
     point = ee.Feature(
         select_point,
         {LABEL: row.Target},
@@ -105,6 +95,7 @@ def labeled_feature(row, iran_collection, sudan_collection, afghanistan_collecti
         .sampleRegions(ee.FeatureCollection([point]), scale=SCALE)
         .first()
     )
+
 
 def sample_cropland_points(scale=500, sample_size=1000):
     """
@@ -119,44 +110,31 @@ def sample_cropland_points(scale=500, sample_size=1000):
 
     """
 
-
     # Load the specified dataset
-    iran_sudan_dataset = get_training_dataset(MODIS_LANDCOVER_ID,
-                                        IRAN_START_DATE, 
-                                        IRAN_END_DATE,
-                                        "LC_Type1")
-    
-    iran_sampled = export_cropland_samples(iran_sudan_dataset,
-                                            iran_geometry,
-                                            12,
-                                            1000,
-                                            500)
+    iran_sudan_dataset = get_training_dataset(
+        MODIS_LANDCOVER_ID, IRAN_START_DATE, IRAN_END_DATE, "LC_Type1"
+    )
+
+    iran_sampled = export_cropland_samples(
+        iran_sudan_dataset, iran_geometry, 12, 1000, 500
+    )
+
+    sudan_sampled = export_cropland_samples(
+        iran_sudan_dataset, sudan_geometry, 12, 1000, 500
+    )
+
+    afghan_dataset = get_training_dataset(
+        DYNAMIC_WORLD_ID, AFGHANISTAN_START_DATE, AFGHANISTAN_END_DATE, "crop"
+    )
+
+    afghan_sampled = export_cropland_samples(
+        afghan_dataset, afghanistan_geometry, 4, 500, 500
+    )
+
+    return iran_sampled, sudan_sampled, afghan_sampled
 
 
-    sudan_sampled = export_cropland_samples(iran_sudan_dataset,
-                                            sudan_geometry,
-                                            12,
-                                            1000,
-                                            500)
-
-    
-    afghan_dataset= get_training_dataset(DYNAMIC_WORLD_ID,
-                                        AFGHANISTAN_START_DATE, 
-                                        AFGHANISTAN_END_DATE,
-                                        "crop")
-    
-    afghan_sampled = export_cropland_samples(afghan_dataset,
-                                            afghanistan_geometry,
-                                            4,
-                                            1000,
-                                            500)
-
-
-    return iran_sampled , sudan_sampled, afghan_sampled
-
-
-
-def export_cropland_samples(image_collection,class_label, region, sample_size, scale):
+def export_cropland_samples(image_collection, class_label, region, sample_size, scale):
     """
     Export cropland samples from an image collection within a specified region.
 
@@ -172,7 +150,9 @@ def export_cropland_samples(image_collection,class_label, region, sample_size, s
     """
 
     # Create a binary mask where cropland pixels are assigned 1 and others as 0
-    cropland_mask = image_collection.eq(class_label)  # 12 represents cropland in the MODIS dataset, 4 for dynamic world
+    cropland_mask = image_collection.eq(
+        class_label
+    )  # 12 represents cropland in the MODIS dataset, 4 for dynamic world
 
     # Convert the binary mask to an image with 1s and 0s
     cropland_image = cropland_mask.rename("Cropland")
@@ -181,24 +161,32 @@ def export_cropland_samples(image_collection,class_label, region, sample_size, s
     cropland_image_clipped = cropland_image.clip(region)
 
     # Create random sample points for cropland (class 1)
-    cropland_samples = cropland_image_clipped.select('Cropland').toInt().stratifiedSample(
-        classBand='Cropland',  # Specify the band used for stratification
-        scale=scale,
-        numPoints=sample_size,
-        region=region,
-        geometries=True
+    cropland_samples = (
+        cropland_image_clipped.select("Cropland")
+        .toInt()
+        .stratifiedSample(
+            classBand="Cropland",  # Specify the band used for stratification
+            scale=scale,
+            numPoints=sample_size,
+            region=region,
+            geometries=True,
+        )
     )
 
     # Create an ID property for each feature
     def add_id(feature):
-        return feature.set("ID", ee.String("ID_").cat(ee.String(ee.Number(feature.id()))))
+        return feature.set(
+            "ID", ee.String("ID_").cat(ee.String(ee.Number(feature.id())))
+        )
 
     samples_with_id = cropland_samples.map(add_id)
 
     # Split the geometry into latitude and longitude
     def split_geometry(feature):
         lat_lon = ee.Geometry(feature.geometry()).coordinates()
-        return feature.setMulti({'Latitude': lat_lon.get(1), 'Longitude': lat_lon.get(0)})
+        return feature.setMulti(
+            {"Latitude": lat_lon.get(1), "Longitude": lat_lon.get(0)}
+        )
 
     samples_with_lat_lon = samples_with_id.map(split_geometry)
 
@@ -220,11 +208,12 @@ def get_training_dataset(id, start_date, end_date, band_name):
                    within the specified date range.
     """
     # Load the specified image collection, filter by date, and select the specified band
-    dataset = ee.ImageCollection(id) \
-        .filterDate(start_date, end_date) \
-        .select(band_name).max()
-    
+    dataset = (
+        ee.ImageCollection(id).filterDate(start_date, end_date).select(band_name).max()
+    )
+
     return dataset
+
 
 def get_collections():
     """
@@ -237,7 +226,7 @@ def get_collections():
     - iran_collection (Sentinel2ImageCollection): A composited Sentinel-2 image collection for Iran.
     - sudan_collection (Sentinel2ImageCollection): A composited Sentinel-2 image collection for Sudan.
     - afghanistan_collection (Sentinel2ImageCollection): A composited Sentinel-2 image collection for Afghanistan.
-    
+
     Note:
     - To use this function, you must have access to the 'create_composited_sentinel2_collection'
       function and the geometries (iran_geometry, sudan_geometry, and afghanistan_geometry)
@@ -249,25 +238,27 @@ def get_collections():
     iran_collection, sudan_collection, afghanistan_collection = get_collections(start_date, end_date)
     # Now you can work with these collections for further analysis or visualization.
     """
-    iran_collection = create_composited_sentinel2_collection(iran_geometry,
-                                                             IRAN_START_DATE,
-                                                             IRAN_END_DATE)
-    sudan_collection = create_composited_sentinel2_collection(sudan_geometry,
-                                                              SUDAN_START_DATE,
-                                                             SUDAN_END_DATE)
-    afghanistan_collection = create_composited_sentinel2_collection(afghanistan_geometry,
-                                                                   AFGHANISTAN_START_DATE,
-                                                                   AFGHANISTAN_END_DATE)
+    iran_collection = create_composited_sentinel2_collection(
+        iran_geometry, IRAN_START_DATE, IRAN_END_DATE
+    )
+    sudan_collection = create_composited_sentinel2_collection(
+        sudan_geometry, SUDAN_START_DATE, SUDAN_END_DATE
+    )
+    afghanistan_collection = create_composited_sentinel2_collection(
+        afghanistan_geometry, AFGHANISTAN_START_DATE, AFGHANISTAN_END_DATE
+    )
     return iran_collection, sudan_collection, afghanistan_collection
 
 
-def select_collection_by_point(point, 
-                               iran_geometry, 
-                               sudan_geometry, 
-                               afghanistan_geometry, 
-                               iran_collection, 
-                               sudan_collection,
-                               afghanistan_collection):
+def select_collection_by_point(
+    point,
+    iran_geometry,
+    sudan_geometry,
+    afghanistan_geometry,
+    iran_collection,
+    sudan_collection,
+    afghanistan_collection,
+):
     """
     Selects an image collection based on whether a given point is within the bounds of a geometry.
 
@@ -301,14 +292,16 @@ def select_collection_by_point(point,
     return selected_collection
 
 
-def create_composited_sentinel2_collection(roi,
-                                           start_date_str, 
-                                           end_date_str, 
-                                           interval=15, 
-                                           limit=10, 
-                                           include_ndvi=True,
-                                           include_ndwi=True, 
-                                           include_evi=True):
+def create_composited_sentinel2_collection(
+    roi,
+    start_date_str,
+    end_date_str,
+    interval=15,
+    limit=10,
+    include_ndvi=True,
+    include_ndwi=True,
+    include_evi=True,
+):
     """
     Creates a 15-day composited Sentinel-2 image collection within the specified ROI and time range.
 
@@ -325,8 +318,8 @@ def create_composited_sentinel2_collection(roi,
         ee.ImageCollection: The composited Sentinel-2 image collection.
     """
     # Convert start_date_str and end_date_str to datetime objects
-    start_date = datetime.strptime(start_date_str, '%Y-%m-%d')
-    end_date = datetime.strptime(end_date_str, '%Y-%m-%d')
+    start_date = datetime.strptime(start_date_str, "%Y-%m-%d")
+    end_date = datetime.strptime(end_date_str, "%Y-%m-%d")
 
     # Create an empty ImageCollection to store the composites
     composited_collection = ee.ImageCollection([])
@@ -339,13 +332,15 @@ def create_composited_sentinel2_collection(roi,
             interval_end_date = end_date
 
         # Format dates as strings
-        interval_start_date_str = start_date.strftime('%Y-%m-%d')
-        interval_end_date_str = interval_end_date.strftime('%Y-%m-%d')
+        interval_start_date_str = start_date.strftime("%Y-%m-%d")
+        interval_end_date_str = interval_end_date.strftime("%Y-%m-%d")
 
         # Filter Sentinel-2 data for the current date range
-        collection = ee.ImageCollection(IMAGE_COLLECTION) \
-            .filterBounds(roi) \
+        collection = (
+            ee.ImageCollection(IMAGE_COLLECTION)
+            .filterBounds(roi)
             .filterDate(interval_start_date_str, interval_end_date_str)
+        )
 
         # Apply the cloud & shadow mask function to the image collection
         collection = collection.map(mask_clouds_and_shadows)
@@ -358,13 +353,15 @@ def create_composited_sentinel2_collection(roi,
         if include_evi:
             collection = collection.map(calculate_evi)
 
-        collection = collection.select([BANDS] +  [FEATURES])
+        collection = collection.select([BANDS] + [FEATURES])
         # Create a median composite of the image collection
         collection = collection.map(normalize_sentinel2)
         median_image = collection.median()
 
         # Add the composite to the ImageCollection
-        composited_collection = composited_collection.merge(ee.ImageCollection([median_image]))
+        composited_collection = composited_collection.merge(
+            ee.ImageCollection([median_image])
+        )
 
         # Move to the next interval
         start_date = interval_end_date + timedelta(days=1)
@@ -372,26 +369,28 @@ def create_composited_sentinel2_collection(roi,
     return composited_collection
 
 
-
-
 def mask_clouds_and_shadows(image):
     """
     Define a function to mask clouds and their shadows.
     Load the Sentinel-2 Cloud Probability (S2C) product.
     """
-    s2c = ee.ImageCollection(SENTINEL_CLOUD_PROB_ID) \
-        .filterBounds(image.geometry()) \
+    s2c = (
+        ee.ImageCollection(SENTINEL_CLOUD_PROB_ID)
+        .filterBounds(image.geometry())
         .filterDate(image.date())
+    )
 
     # Select the cloud probability band.
-    cloud_probability = s2c.first().select('probability')
+    cloud_probability = s2c.first().select("probability")
 
     # Create a cloud mask by thresholding the cloud probability.
     cloud_mask = cloud_probability.lt(CLOUD_THRESHOLD)
 
     # Create a shadow mask using the cloud probability and Sentinel-2 image's cirrus band (B10).
-    cloud_displacement = image.select('B10').subtract(cloud_probability).abs()
-    shadow_mask = cloud_displacement.lt(CLOUD_DISPLACEMENT_THRESHOLD)  # Adjust the threshold as needed.
+    cloud_displacement = image.select("B10").subtract(cloud_probability).abs()
+    shadow_mask = cloud_displacement.lt(
+        CLOUD_DISPLACEMENT_THRESHOLD
+    )  # Adjust the threshold as needed.
 
     # Combine the cloud mask and shadow mask.
     final_mask = cloud_mask.And(shadow_mask)
@@ -400,7 +399,6 @@ def mask_clouds_and_shadows(image):
     masked_image = image.updateMask(final_mask)
 
     return masked_image
-
 
 
 def normalize_sentinel2(image):
@@ -415,34 +413,32 @@ def normalize_sentinel2(image):
 
 def mask_clouds(image):
     """
-          Create a function to mask clouds using the Sentinel-2 QA60 band
+    Create a function to mask clouds using the Sentinel-2 QA60 band
     """
 
-    QA60 = image.select(['QA60'])
+    QA60 = image.select(["QA60"])
     cloud_mask = QA60.bitwiseAnd(1 << 10).eq(0).And(QA60.bitwiseAnd(1 << 11).eq(0))
     return image.updateMask(cloud_mask)
 
+
 def calculate_ndvi(image):
     """Calculate NDVI for an image."""
-    ndvi = image.normalizedDifference(['B8', 'B4']).rename('NDVI')
+    ndvi = image.normalizedDifference(["B8", "B4"]).rename("NDVI")
     return image.addBands(ndvi)
+
 
 def calculate_ndwi(image):
     """Calculate NDWI for an image"""
-    nir_band = 'B8'  # Near-Infrared (NIR) band
-    swir_band = 'B11'  # Shortwave Infrared (SWIR) band
-    ndwi = image.normalizedDifference([nir_band, swir_band]).rename('NDWI')
+    nir_band = "B8"  # Near-Infrared (NIR) band
+    swir_band = "B11"  # Shortwave Infrared (SWIR) band
+    ndwi = image.normalizedDifference([nir_band, swir_band]).rename("NDWI")
     return image.addBands(ndwi)
-
 
 
 def calculate_evi(image):
     """Calculate EVI for an image."""
     evi = image.expression(
-        '2.5 * ((B8 - B4) / (B8 + 6 * B4 - 7.5 * B2 + 1))', {
-            'B8': image.select('B8'),
-            'B4': image.select('B4'),
-            'B2': image.select('B2')
-        }
-    ).rename('EVI')
+        "2.5 * ((B8 - B4) / (B8 + 6 * B4 - 7.5 * B2 + 1))",
+        {"B8": image.select("B8"), "B4": image.select("B4"), "B2": image.select("B2")},
+    ).rename("EVI")
     return image.addBands(evi)
