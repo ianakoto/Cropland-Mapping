060102299b6d2d226ed6bfe329eaa4ef42f18ce9
Ian Cecil Mawuli Akoto <iancecilakoto@gmail.com>
2023-09-14 14:20:15 +0000

    earth engine credential

--- serving/data.py
+++ serving/data.py
@@ -1,8 +1,36 @@
+from __future__ import annotations
+
+
 from datetime import datetime, timedelta
 import ee
 import numpy as np
 from config import *
 import time
+
+from sklearn.model_selection import train_test_split
+from google.api_core import exceptions, retry
+import google.auth
+
+
+
+
+# Authenticate and initialize Earth Engine with the default credentials.
+credentials, project = google.auth.default(
+    scopes=[
+        "https://www.googleapis.com/auth/cloud-platform",
+        "https://www.googleapis.com/auth/earthengine",
+    ]
+)
+
+# Use the Earth Engine High Volume endpoint.
+#   https://developers.google.com/earth-engine/cloud/highvolume
+ee.Initialize(
+    credentials.with_quota_project(None),
+    project=project,
+    opt_url="https://earthengine-highvolume.googleapis.com",
+)
+
+
 
 countries = ee.FeatureCollection("FAO/GAUL/2015/level2")
 
--- serving/setup.py
+++ serving/setup.py
@@ -10,6 +10,10 @@ setup(
     install_requires=[
     "earthengine-api==0.1.336",
     "tensorflow==2.9.0",
+    "sklearn==0.24.2",
+    "google-api-core==2.0.1",
+    "google-auth==2.0.2",
+    "apache-beam[gcp]==2.36.0",
     "IPython"
     ],
 )
